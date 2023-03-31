c54a5a70c5e7922232b8131321a90d68105463f6
taylorcovington <tcovingtondev@gmail.com>
2023-03-31 15:34:42 -0600

    Initial Commit

---
+++ MacroFlexV1/MacroFlexV1App.swift
@@ -0,0 +1,17 @@
+//
+//  MacroFlexV1App.swift
+//  MacroFlexV1
+//
+//  Created by Taylor Covington on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroFlexV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
